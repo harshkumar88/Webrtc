67d38cc59367cde2d6c38afb41f82e20ee205410
harsh kumar <harsh.kumar@crofarm.in>
2024-10-17 20:39:25 +0530

    shn

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const io = require("socket.io")(server, {
     ],
   },
 });
-const PORT = 3000 || process.env.PORT;
+const PORT = 5000;
 
 io.on("connection", (socket) => {
   console.log("connect server");
